a062ebff076496d92698f835d87021416727d78d
Meysam <mushroom.1092@gmail.com>
2024-12-18 09:55:47 +0330

    Update and rename firewall3 to firewall3.sh

--- firewall3.sh
+++ firewall3.sh
@@ -48,7 +48,7 @@ function install_and_configure {
     ufw deny 166
     ufw deny 78
 
-    # Add missing rules from the list
+   # Add missing rules from the list
     ufw deny to 200.0.0.0/8
     ufw deny to 102.0.0.0/8
     ufw deny to 192.0.0.0/24
@@ -66,8 +66,95 @@ function install_and_configure {
     ufw deny to 224.0.0.0/4
     ufw deny to 240.0.0.0/4
     ufw deny to 255.255.255.255/32
+    ufw deny to 102.94.62.0/24
+    ufw deny out from any to 10.0.0.0/8
+    ufw deny out from any to 172.16.0.0/12
+    ufw deny out from any to 192.168.0.0/16
+    ufw deny out from any to 100.64.0.0/10
+    ufw deny out from any to 169.254.0.0/16
+    ufw deny out from any to 198.18.0.0/15
+    ufw deny out from any to 102.236.0.0/16
+    ufw deny out from any to 2.60.0.0/16
+    ufw deny out from any to 5.1.41.0/12
 
     # IPTables configuration
+    sudo iptables -A FORWARD -s 200.0.0.0/8 -j DROP
+     sudo iptables -A FORWARD -s 200.0.0.0/8 -j DROP
+    sudo iptables -A FORWARD -s 102.0.0.0/8 -j DROP
+    sudo iptables -A FORWARD -s 10.0.0.0/8 -j DROP
+    sudo iptables -A FORWARD -s 100.64.0.0/10 -j DROP
+    sudo iptables -A FORWARD -s 169.254.0.0/16 -j DROP
+    sudo iptables -A FORWARD -s 198.18.0.0/15 -j DROP
+    sudo iptables -A FORWARD -s 198.51.100.0/24 -j DROP
+    sudo iptables -A FORWARD -s 203.0.113.0/24 -j DROP
+    sudo iptables -A FORWARD -s 224.0.0.0/4 -j DROP
+    sudo iptables -A FORWARD -s 240.0.0.0/4 -j DROP
+    sudo iptables -A FORWARD -s 255.255.255.255/32 -j DROP
+    sudo iptables -A FORWARD -s 192.0.0.0/24 -j DROP
+    sudo iptables -A FORWARD -s 192.0.2.0/24 -j DROP
+    sudo iptables -A FORWARD -s 127.0.0.0/8 -j DROP
+    sudo iptables -A FORWARD -s 127.0.53.53 -j DROP
+    sudo iptables -A FORWARD -s 192.168.0.0/16 -j DROP
+    sudo iptables -A FORWARD -s 0.0.0.0/8 -j DROP
+    sudo iptables -A FORWARD -s 172.16.0.0/12 -j DROP
+    sudo iptables -A FORWARD -s 224.0.0.0/3 -j DROP
+    sudo iptables -A FORWARD -s 192.88.99.0/24 -j DROP
+    sudo iptables -A FORWARD -s 169.254.0.0/16 -j DROP
+    sudo iptables -A FORWARD -s 198.18.140.0/24 -j DROP
+    sudo iptables -A FORWARD -s 102.230.9.0/24 -j DROP
+    sudo iptables -A FORWARD -s 102.233.71.0/24 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 102.0.0.0/8 -j DROP
+    sudo ptables -A OUTPUT   -s 0/0 -d 102.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 185.166.104.4/16 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 185.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 184.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 185.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 216.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 188.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 173.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 172.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 157.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 149.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 151.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 142.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 138.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 130.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 114.0.0.0/8 -j DROP
+    sudo iptables -A OUTPUT  -s 0/0 -d 108.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 104.0.0.0/10 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 95.0.0.0/10 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 94.0.0.0/10 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 91.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 86.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 79.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 74.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 71.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 66.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 64.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 54.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 48.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 47.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 46.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 45.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 44.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 49.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 35.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 31.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 23.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 20.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 18.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 17.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 16.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 15.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 13.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 5.0.0.0/8 -j DROP
+    iptables -A OUTPUT -s 0/0 -d 3.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 2.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 185.166.104.4/16 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 16.0.0.0/8 -j DROP
+    iptables -A OUTPUT  -s 0/0 -d 15.0.0.0/8 -j DROP
+  iptables -A OUTPUT  -s 0/0 -d 5.0.0.0/8 -j DROP
+ iptables -A OUTPUT  -s 0/0 -d 3.0.0.0/8 -j DRO
     sudo iptables -A INPUT -p tcp --dport 302 -j DROP
     sudo iptables -A INPUT -p tcp --dport 166 -j DROP
     sudo iptables -A INPUT -p tcp --dport 78 -j DROP
@@ -77,6 +164,7 @@ function install_and_configure {
     echo "Configuration completed. Press Enter to return to menu."
     read
 }
+
 
 # Disable UFW and Clear IPTables
 function disable_and_clear {
